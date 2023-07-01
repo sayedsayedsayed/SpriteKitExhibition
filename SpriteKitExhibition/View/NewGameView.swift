972bebad9997aff739763a4d658be52cdb625eff
asadiiefji <asadiiefji@gmail.com>
2023-07-01 22:14:18 +0700

    add controller of danugamescene

--- SpriteKitExhibition/View/NewGameView.swift
+++ SpriteKitExhibition/View/NewGameView.swift
@@ -34,6 +34,10 @@ struct NewGameView: View {
                         .overlay(
                             NavigationLink("Asadi"){
                                 DanuView()
+                                    .onAppear {
+                                        let hostingController = HostingController(rootView: DanuView())
+                                        UIApplication.shared.windows.first?.rootViewController = hostingController
+                                    }
                             }
                                 .font(.largeTitle)
                                 .fontWeight(.bold)
