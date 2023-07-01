f25f7d0abb095e0baf4f6740a45b9dff48341989
Sayed Zulfikar <74494579+sayedsayedsayed@users.noreply.github.com>
2023-07-01 20:18:07 +0700

    Initial Commit

---
+++ SpriteKitExhibition/SpriteKitExhibitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitExhibitionApp.swift
+//  SpriteKitExhibition
+//
+//  Created by Sayed Zulfikar on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitExhibitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
